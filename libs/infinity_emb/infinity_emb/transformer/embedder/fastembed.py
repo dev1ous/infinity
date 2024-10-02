70c6afd3457010da29a9afd8c87df054051f5d84
dev1ous <60881111+dev1ous@users.noreply.github.com>
2024-10-03 01:24:01 +0200

    Update fastembed.py

--- libs/infinity_emb/infinity_emb/transformer/embedder/fastembed.py
+++ libs/infinity_emb/infinity_emb/transformer/embedder/fastembed.py
@@ -7,7 +7,7 @@ from infinity_emb.primitives import Device, EmbeddingReturnType, PoolingMethod
 from infinity_emb.transformer.abstract import BaseEmbedder
 try:
     from fastembed import SparseTextEmbedding
-    from infinity_emb.transformer.utils_optimum import normalize
+    from fastembed.common.onnx_model import OnnxOutputContext
     FASTEMBED_AVAILABLE = True
 except ImportError:
     FASTEMBED_AVAILABLE = False
@@ -36,11 +36,11 @@ class Fastembed(BaseEmbedder):
         self._infinity_tokenizer = copy.deepcopy(self.model.tokenizer)
     def encode_pre(self, sentences: List[str]) -> Dict[str, np.ndarray]:
         encoded = self.model.tokenizer.encode_batch(sentences)
-        input_ids = np.array([e.ids for e in encoded])
-        attention_mask = np.array([e.attention_mask for e in encoded])
+        self.input_ids = np.array([e.ids for e in encoded])
+        self.attention_mask = np.array([e.attention_mask for e in encoded])
         onnx_input = {
-            "input_ids": np.array(input_ids, dtype=np.int64),
-            "attention_mask": np.array(attention_mask, dtype=np.int64),
+            "input_ids": np.array(self.input_ids, dtype=np.int64),
+            "attention_mask": np.array(self.attention_mask, dtype=np.int64),
             "token_type_ids": np.array(
                 [np.zeros(len(e), dtype=np.int64) for e in input_ids], dtype=np.int64
             ),
@@ -48,10 +48,33 @@ class Fastembed(BaseEmbedder):
         return onnx_input
     def encode_core(self, features: Dict[str, np.ndarray]) -> np.ndarray:
         model_output = self.model.model.run(None, features)
-        last_hidden_state = model_output[0][:, 0]
-        return last_hidden_state
+        return model_output[0]
     def encode_post(self, embedding: np.ndarray) -> EmbeddingReturnType:
-        return normalize(embedding).astype(np.float32)
+        token_ids_batch = self.input_ids
+
+        pooled_attention = np.mean(embedding[:, :, 0], axis=1) * self.attention_mask
+
+        for document_token_ids, attention_value in zip(token_ids_batch, pooled_attention):
+            document_tokens_with_ids = (
+                (idx, self.model.invert_vocab[token_id])
+                for idx, token_id in enumerate(document_token_ids)
+            )
+
+            reconstructed = self.model._reconstruct_bpe(document_tokens_with_ids)
+
+            filtered = self.model._filter_pair_tokens(reconstructed)
+
+            stemmed = self.model._stem_pair_tokens(filtered)
+
+            weighted = self.model._aggregate_weights(stemmed, attention_value)
+
+            max_token_weight = {}
+
+            for token, weight in weighted:
+                max_token_weight[token] = max(max_token_weight.get(token, 0), weight)
+
+            rescored = self.model._rescore_vector(max_token_weight)
+
     def tokenize_lengths(self, sentences: List[str]) -> List[int]:
         tks = self._infinity_tokenizer.encode_batch(
             sentences,
